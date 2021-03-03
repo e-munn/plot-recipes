3aef431247b9118a68a130569044b770a2545717
EMUNN <eli.luke@gmail.com>
2021-03-03 00:10:14 -0500

    home

--- src/components/card.js
+++ src/components/card.js
@@ -6,6 +6,8 @@ import '../css/cards.css'
 import creamy from '../media/images/creamy-white-bean-soup-with-spicy-paprika-oil.jpg'
 import hearty from '../media/images/hearty-whole-wheat-pasta-with-brussels-sprouts-cheese-and-potato.jpg'
 import sweet from '../media/images/sweet-potatoes-with-cilantro-chiles-sauce.jpg'
+import plate1 from '../media/images/plate1.png'
+
 
 
 const Card = ({d, i}) => {
@@ -19,15 +21,20 @@ const Card = ({d, i}) => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
   const [windowHeight, setWindowHeight] = useState(window.innerHeight)
 
-  var url = '../media/images/' + d.path + '.jpg'
+  // var url = '../media/images/' + d.path + '.jpg'
+
   // var url = '../media/images/creamy-white-bean-soup-with-spicy-paprika-oil.jpg'
 
 
   return (
     <div
+      className={'card-holder'}
+      >
+    <div
       className={'card'}
       style={{
-        // backgroundImage: `url(${imageMap(d.path)})`
+        backgroundImage: `url(${(plate1)})`
+        // backgroundImage: `url(${ url })`
       }}
       >
       <div className={'card-cover'}>
@@ -38,6 +45,8 @@ const Card = ({d, i}) => {
         </div>
       </div>
     </div>
+  </div>
+    
   )
 }
 
